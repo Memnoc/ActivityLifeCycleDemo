2eb077b0abe0f20c75e85ae6dcdb45028c5bb5d7
Memnoc <smartdroidesign@gmail.com>
2018-10-31 15:45:45 +0000

    Initial commit

---
+++ app/src/main/java/com/smartdroidesign/activitylifecycledemo/OpaqueActivity.java
@@ -0,0 +1,17 @@
+package com.smartdroidesign.activitylifecycledemo;
+
+
+import android.os.Bundle;
+
+public class OpaqueActivity extends LoggingActivity {
+
+    public OpaqueActivity() {
+        setActivityName("OpaqueActivity");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opaque);
+    }
+}
